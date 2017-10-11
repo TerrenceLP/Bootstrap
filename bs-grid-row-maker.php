4ea68807e133fe284da50c739407bc2c716b7469
Terrence Porretto <terrence@porretto.com>
2017-10-11 16:29:26 -0400

    Update bs-grid-row-maker.php

--- bs-grid-row-maker.php
+++ bs-grid-row-maker.php
@@ -3,11 +3,11 @@ $parties = array("foo", "bar", "hello", "world", "xxx", "876", "777");
 $rowgrid = '<div class="row">';
 $count = 0;
 $numOfCols = 3;
-$bootstrapColWidth = 12 / $numOfCols;
+$bootstrapColWidth = 4 / $numOfCols;
 
         foreach ($parties as $key => $value) {
             
-              $rowgrid .= '<div class="col-md-4">'.$value.' <br> Title of Party <br> Event Date</div>';
+              $rowgrid .= '<div class="col-md-'.$bootstrapColWidth.'">'.$value.' <br> Title of Party <br> Event Date</div>';
             
             $count++;
             if ($count % $numOfCols == 0) {
