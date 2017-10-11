8f752c5aed3d61e8380c331a1da5d6aedf1a1e16
Terrence Porretto <terrence@porretto.com>
2017-10-11 16:25:44 -0400

    Create New File
    Bootstrap Looping Grid Maker

---
+++ bs-grid-row-maker.php
@@ -0,0 +1,21 @@
+<?php
+$parties = array("foo", "bar", "hello", "world", "xxx", "876", "777");
+$rowgrid = '<div class="row">';
+$count = 0;
+$numOfCols = 3;
+$bootstrapColWidth = 12 / $numOfCols;
+
+        foreach ($parties as $key => $value) {
+            
+              $rowgrid .= '<div class="col-md-4">'.$value.' <br> Title of Party <br> Event Date</div>';
+            
+            $count++;
+            if ($count % $numOfCols == 0) {
+              $rowgrid .= '</div><div class="row">';
+            }
+            
+        }
+
+$rowgrid .= '</div>';
+echo $rowgrid;
+?>
